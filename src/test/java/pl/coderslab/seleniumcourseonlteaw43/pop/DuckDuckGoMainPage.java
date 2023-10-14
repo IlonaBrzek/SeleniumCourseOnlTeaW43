ea7765a2f241cb303b69fe9bb1d7d4e8f9d37b0b
edu <edu@mail.pl>
2023-10-14 11:47:00 +0200

    d5t2 add new method fillSearchInputAndEnter to DdgMainPage

--- src/test/java/pl/coderslab/seleniumcourseonlteaw43/pop/DuckDuckGoMainPage.java
+++ src/test/java/pl/coderslab/seleniumcourseonlteaw43/pop/DuckDuckGoMainPage.java
@@ -1,6 +1,7 @@
 package pl.coderslab.seleniumcourseonlteaw43.pop;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
@@ -14,6 +15,11 @@ public class DuckDuckGoMainPage {
     public void fillSearchInput(String searchPhrase) {
         WebElement searchInput = driver.findElement(By.id("searchbox_input"));
         searchInput.sendKeys(searchPhrase);
+    }
+
+    public void fillSearchInputAndEnter(String searchPhrase) {
+        WebElement searchInput = driver.findElement(By.id("searchbox_input"));
+        searchInput.sendKeys(searchPhrase + Keys.ENTER);
     }
 
     public void clickSearchButton() {
