0f0444cb113025e3215eebec1635aa62a8e4b8d9
edu <edu@mail.pl>
2023-10-14 09:53:41 +0200

    d5t1 DuckDuckGoMainPage model and refactor test 1

---
+++ src/test/java/pl/coderslab/seleniumcourseonlteaw43/pop/DuckDuckGoMainPage.java
@@ -0,0 +1,23 @@
+package pl.coderslab.seleniumcourseonlteaw43.pop;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class DuckDuckGoMainPage {
+    private WebDriver driver;
+
+    public DuckDuckGoMainPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void fillSearchInput(String searchPhrase) {
+        WebElement searchInput = driver.findElement(By.id("searchbox_input"));
+        searchInput.sendKeys(searchPhrase);
+    }
+
+    public void clickSearchButton() {
+        WebElement searchButton = driver.findElement(By.className("searchbox_searchButton__F5Bwq"));
+        searchButton.click();
+    }
+}
---
+++ src/test/java/pl/coderslab/seleniumcourseonlteaw43/pop/DuckDuckGoSearchPopTest.java
@@ -0,0 +1,34 @@
+package pl.coderslab.seleniumcourseonlteaw43.pop;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DuckDuckGoSearchPopTest {
+    @Test
+    public void searchWithDdg() {
+        // given
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://duckduckgo.com/");
+        // when
+        DuckDuckGoMainPage ddgMainPage = new DuckDuckGoMainPage(driver);
+        ddgMainPage.fillSearchInput("w pustyni i w puszczy");
+        ddgMainPage.clickSearchButton();
+        // then
+        // assert
+        //driver.quit();
+    }
+
+    @Test
+    public void searchWithDdgEnter() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://duckduckgo.com/");
+        WebElement searchInput = driver.findElement(By.id("searchbox_input"));
+        searchInput.sendKeys("w pustyni i w puszczy" + Keys.ENTER);
+
+//        searchInput.sendKeys(Keys.ENTER);
+    }
+}
