a8ae1e9ce16883001f09b4de7c8e528730ea9c09
edu <edu@mail.pl>
2023-10-15 14:31:15 +0200

    d6t2 save screenshot

--- src/cucumber/features/search-with-duck-duck-go.feature
+++ src/cucumber/features/search-with-duck-duck-go.feature
@@ -5,6 +5,7 @@ Given Page https://duckduckgo.com/ opened in browser
 When Phrase '<searchPhrase>' entered in search input box
 And Search button clicked
 Then First <meaningfulPositions> search result text contain phrase '<searchPhrase>'
+And Save screenshot
 And Close browser
 
   Examples:
--- src/test/java/pl/coderslab/seleniumcourseonlteaw43/cucumber/SearchWithDuckDuckGoSteps.java
+++ src/test/java/pl/coderslab/seleniumcourseonlteaw43/cucumber/SearchWithDuckDuckGoSteps.java
@@ -5,12 +5,19 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import pl.coderslab.seleniumcourseonlteaw43.pop.DuckDuckGoMainPage;
 import pl.coderslab.seleniumcourseonlteaw43.pop.DuckDuckGoSearchResultsPage;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class SearchWithDuckDuckGoSteps {
@@ -51,9 +58,20 @@ public class SearchWithDuckDuckGoSteps {
         }
     }
 
+    @And("Save screenshot")
+    public void saveScreenshot() throws IOException {
+        //Take screenshot (will be saved in default location) and automatically removed after test
+        File tmpScreenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        //Copy the screenshot to desired location
+        //Path to the location to save screenshot
+        //(directory for screenshots MUST exist: C:\test-evidence) e.g.:
+        String currentDateTime = LocalDateTime.now().toString().replaceAll(":", "_");
+        // new three classes that we use below: Files, Path, Paths
+        Files.copy(tmpScreenshot.toPath(), Paths.get("C:", "test-evidence", "ddg-search-"+currentDateTime+".png"));
+    }
+
     @And("Close browser")
     public void closeBrowser() {
-        System.out.println("close browser!");
-//        this.driver.quit();
+        this.driver.quit();
     }
 }
